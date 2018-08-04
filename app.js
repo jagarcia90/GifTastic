36d53d7eceaedaebeec3eee027340420110eb0cb
Jorge Garcia <jagarcia@ucdavis.edu>
2018-08-03 19:06:21 -0700

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var topics = ['dog', 'cat', 'jaguar', 'wolf'];
 
     //  create topics array buttons
-    function buttonExpress() {
+    function buttonAdd() {
         $('#buttonsView').empty();
 
         for (var i = 0; i < topics.length; i++) {
@@ -14,28 +14,27 @@ $(document).ready(function () {
             $('#buttonsView').append(a);
         }
     }
-    buttonExpress();
-
-
+    buttonAdd();
     //on button click
     $(document).on('click', '.expression', function () {
 
-        var car = $(this).html();
-        console.log(car);
+        var animal = $(this).html();
 
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + car + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=RKCuRKKH4MjwJlCPJ2ZVV3v6YjAuZem0&limit=10";
         $.ajax({ url: queryURL, method: 'GET' })
             .done(function (response) {
+
                 // grabs the data
                 var results = response.data;
+
                 //empties the div before adding more gifs
                 $('#expressView').empty();
+
                 //loops through the data
                 for (var j = 0; j < results.length; j++) {
                     var imageDiv = $('<div>');
                     var imageView = results[j].images.fixed_height.url;
                     var still = results[j].images.fixed_height_still.url;
-                    // console.log(imageView);  
                     var expressImage = $('<img>').attr("src", still).attr('data-animate', imageView).attr('data-still', still);
                     expressImage.attr('data-state', 'still');
                     $('#expressView').prepend(expressImage);
@@ -43,15 +42,13 @@ $(document).ready(function () {
 
                     // pulling the rating
                     var rating = results[j].rating;
-                    // console.log(rating);
                     var displayRated = $('<p>').text("Rating: " + rating);
                     $('#expressView').prepend(displayRated);
-
                 }
             });
-        function playGif() {
+        //Adds multiple Gifs 
+            function playGif() {
             var state = $(this).attr('data-state');
-            console.log(state);
             if (state == 'still') {
                 $(this).attr('src', $(this).data('animate'));
                 $(this).attr('data-state', 'animate');
@@ -59,30 +56,19 @@ $(document).ready(function () {
                 $(this).attr('src', $(this).data('still'));
                 $(this).attr('data-state', 'still');
             }
-
-        } //on click express
-
-    }) // document on click
-
-
-
-
+        } 
+    }) 
     //adding new button
-    $(document).on('click', '#addExpress', function () {
+    $(document).on('click', '#addGifphy', function () {
         if ($('#express-input').val().trim() == '') {
-            alert('please add car');
+            alert('please add animal');
         }
         else {
-            var car = $('#express-input').val().trim();
-            topics.push(car);
+            var animal = $('#express-input').val().trim();
+            topics.push(animal);
             $('#express-input').val('');
-            buttonExpress();
+            buttonAdd();
             return false;
-
         }
-
     });
-
-
-
 });
